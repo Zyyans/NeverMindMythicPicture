c4d3d49a933166c78998ffe2e927e7d1a565e238
蠢驴 <1980797568@qq.com>
2020-11-30 22:07:41 +0800

    给Base类添加功能
    纠结易读性真是快乐而痛苦

--- classes.py
+++ classes.py
@@ -6,9 +6,34 @@ class Base:
 
     def __init__(self):
 
-        self.path = Path.cwd()
-        for sub_path in ["Pictures", "Textures"]:
-            (self.path / sub_path).mkdir(exist_ok=True)
+        self.path = Path('.')
+        self.pic_dir_path = self.path / "Pictures"
+        self.tex_dir_path = self.path / "Textures"
+
+        self.pic_dir_path.mkdir(exist_ok=True)
+        self.tex_dir_path.mkdir(exist_ok=True)
+    
+    def get_pic_paths(self):
+
+        self.pic_paths = []
+        for suffix in ["gif", "jpg", "png"]:
+            for path in self.pic_dir_path.glob("*." + suffix):
+                self.pic_paths.append(path)
+        return len(self.pic_paths) if self.pic_paths else False
+
+    def get_tex_paths(self):  # 待优化
+
+        json_paths = [path for path in self.tex_dir_path.glob("*.json")]
+        png_paths = [path for path in self.tex_dir_path.glob("*.png")]
+
+        json_stems = [path.stem for path in json_paths]
+        if json_stems != [path.stem for path in png_paths]:
+            return False
+
+        self.tex_paths = []
+        for i in range(len(json_stems)):
+            self.tex_paths.append({json_stems[i]: (json_paths[i], png_paths[i])})
+        return len(json_stems)
 
 
 class Picture:
@@ -24,9 +49,6 @@ class Picture:
     def width(self):
         return self.image.width
 
-    @property
-    def height(self):
-        return self.image.height
 
     @property
     def pixel(self, x, y):
@@ -65,5 +87,5 @@ class GIF(Picture):
         super().__init__()
 
 
-temp = Picture("icon.jpg")
-print(temp.width, temp.height)
+base = Base()
+print(base.get_tex_paths())
